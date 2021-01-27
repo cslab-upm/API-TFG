06ee80fab184c6ac1b4931a1f3d31aaf568d1fe1
Marcos Gamazo <gamazoruiz@gmail.com>
2021-01-27 16:59:21 +0100

    path update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const swaggerOptions = {
         },
         layout: "OperationsLayout",
         servers:[
-            {url:'http://localhost:3001/'},
+            {url:'http://localhost:3000/'},
         ]
     },
     apis:['index.js','./routes/*.js']
